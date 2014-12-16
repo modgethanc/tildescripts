859dd4338e8ae5ce58307bc3e27e67e6bfe02af1
endorphant <endorphant@ctrl-c.club>
2014-12-16 01:13:08 -0500

    first commit

---
+++ nethackboard.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+use strict;
+
+
+open IN, '/var/games/nethack/logfile';
+my @log = <IN>;
+close IN;
+
+open OUT, ">", "www/nethacklog.html";
+select OUT;
+#print @log;
+
+print "<ol>\n";
+
+foreach (@log) {
+	my @line = split(' ', $_);
+	shift(@line);
+	print "<li>";
+	print join(' ', @line);
+	print "</li>\n";
+}
+
+print "</ol>";
+
+close OUT;
