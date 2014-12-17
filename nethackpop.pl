b02e2dd3be56e26c135496bc559030dc73712184
endorphant <endorphant@ctrl-c.club>
2014-12-16 23:23:21 -0500

    added races

--- nethackpop.pl
+++ nethackpop.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #use strict;
-
+use feature qw(switch);
 
 open IN, '/var/games/nethack/logfile';
 my @log = <IN>;
@@ -33,19 +33,31 @@ foreach (@log) {
 
 	$empires{$user}[0]++;
 	
-	if ($gender =~ /Mal/) { $empires{$user}[1]++; } 
-		else { $empires{$user}[2]++; }
+	given ($gender) {
+		when (/Mal/) { $empires{$user}[1]++ ;}
+		when (/Fem/) { $empires{$user}[2]++ ;}
+	}
 
-	if ($align =~ /Cha/) { $empires{$user}[3]++; }
-		elsif ($align =~ /Neu/) { $empires{$user}[4]++; }
-		else { $empires{$user}[5]++; }
+	given ($align) {
+		when (/Cha/) { $empires{$user}[3]++ ;}
+		when (/Neu/) { $empires{$user}[4]++ ;}
+		when (/Law/) { $empires{$user}[5]++ ;}
+	}
+	
+	given ($race) {
+		when (/Hum/) { $empires{$user}[6]++ ;}
+		when (/Dwa/) { $empires{$user}[7]++ ;}
+		when (/Elf/) { $empires{$user}[8]++ ;}
+		when (/Gno/) { $empires{$user}[9]++ ;}
+		when (/Orc/) { $empires{$user}[10]++ ;}
+	}	
+
 }
 
 print "<ul>\n";
 
 for (keys %empires) {
 	my $alignment = "divided";
-	my $civ = "venture";
 	
 	if (($empires{$_}[3] > $empires{$_}[4]) && ($empires{$_}[3] > $empires{$_}[5])) {
 		$alignment = "chaotic";
@@ -58,14 +70,26 @@ for (keys %empires) {
 	if (($empires{$_}[5] > $empires{$_}[4]) && ($empires{$_}[5] > $empires{$_}[5])) {
 		$alignment = "lawful";
 	}
-	
+
+	my $civ = "venture";
+
 	if ($empires{$_}[0] > 1) { $civ = "party"; }
 	if ($empires{$_}[0] > 4) { $civ = "gang"; }
 	if ($empires{$_}[0] > 11) { $civ = "patrol"; }
 	if ($empires{$_}[0] > 24) { $civ = "outpost"; }
 	if ($empires{$_}[0] > 39) { $civ = "village"; }
 
-	print "<li><b>the $alignment $civ of <a href=\"../~$_\">~$_</a></b><br />\n
+	my @races;
+
+	if ($empires{$_}[6] > 0) { push(@races, "human"); }
+	if ($empires{$_}[7] > 0) { push(@races, "dwarven"); }
+	if ($empires{$_}[8] > 0) { push(@races, "elven"); }
+	if ($empires{$_}[9] > 0) { push(@races, "gnomish"); }
+	if ($empires{$_}[10] > 0) { push(@races, "orcish"); }
+
+	my $lineup = join("/", @races);
+
+	print "<li><b>the $alignment $lineup $civ of <a href=\"../~$_\">~$_</a></b><br />\n
 		 pop. $empires{$_}[0]<br />\n
 		gender ratio: $empires{$_}[1]:$empires{$_}[2]</li>\n";
 	print "<br />\n";
